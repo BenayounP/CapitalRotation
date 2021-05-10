044a5ea3ceba2411de63391d9e483913b4901eb8
PierreB <pierre@cabnum.fr>
2021-05-10 12:38:29 +0200

    step Check finished

--- app/src/main/java/eu/pbenayoun/capitalrotation/presentation/home/HomeFragment.kt
+++ app/src/main/java/eu/pbenayoun/capitalrotation/presentation/home/HomeFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doAfterTextChanged
+import androidx.databinding.adapters.TextViewBindingAdapter.setText
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.google.android.material.snackbar.Snackbar
@@ -31,6 +33,11 @@ class HomeFragment() : Fragment(R.layout.fragment_home) {
         return view
     }
 
+    override fun onResume() {
+        super.onResume()
+        setTexts()
+    }
+
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
@@ -40,9 +47,33 @@ class HomeFragment() : Fragment(R.layout.fragment_home) {
 
     }
 
+    private fun setTexts(){
+        binding.homeTitle.text=getString(viewModel.stepTextIds.title)
+        binding.homeEditSearch.setText(viewModel.currentQueryText)
+        binding.homeEditSearch.hint=getString(viewModel.stepTextIds.editTextHint)
+        binding.homeButton.text=getString(viewModel.stepTextIds.buttonText)
+    }
+
 
     private fun setViews() {
+        binding.homeEditSearch.doAfterTextChanged {
+            viewModel.setCurrentQuery(it.toString())
+        }
 
+        binding.homeButton.setOnClickListener { view ->
+            onButtonClick()
+        }
+    }
+
+    fun onButtonClick(){
+        when(viewModel.checkQuery()){
+             CheckResult.OK-> setTexts()
+            CheckResult.KO ->{
+                binding.homeEditSearch.setText(viewModel.currentQueryText)
+                snackIt(getString(viewModel.stepTextIds.snackText))
+            }
+
+        }
     }
 
 
--- app/src/main/java/eu/pbenayoun/capitalrotation/presentation/home/HomeViewModel.kt
+++ app/src/main/java/eu/pbenayoun/capitalrotation/presentation/home/HomeViewModel.kt
@@ -1,13 +1,60 @@
 package eu.pbenayoun.capitalrotation.presentation.home
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import eu.pbenayoun.capitalrotation.R
+
+enum class Step{
+    CHECK_CAPITAL_LETTER,
+    ROTATION_ANGLE
+}
+
+enum class CheckResult{
+    OK,
+    KO
+}
+
+data class TextsId(val title:Int, val editTextHint:Int, val buttonText: Int, val snackText: Int)
+
+
 
 class HomeViewModel : ViewModel() {
 
+    val CHECK_STEP_TEXTSID = TextsId(R.string.home_check_capital_title,R.string.home_check_capital_hint,R.string.home_button_check,R.string.home_capital_snack_text_id)
+    val ROTATION_STEP_TEXTSID = TextsId(R.string.home_rotation_title,R.string.home_rotation_hint,R.string.home_button_rotate,R.string.home_rotation_snack_text_id)
+    var currentStep = Step.CHECK_CAPITAL_LETTER
+    var stepTextIds= CHECK_STEP_TEXTSID
 
-    fun CheckCapitalLetterPrefix(string:String):Boolean{
+    var currentQueryText  = ""
+
+    fun setCurrentQuery(newQuery : String){
+        currentQueryText = newQuery
+    }
+
+    fun checkQuery() : CheckResult{
+        return checkCapitalQuery()
+    }
+
+    private fun checkCapitalQuery() : CheckResult{
+        val checkResult = checkCapitalLetterPrefix(currentQueryText)
+        currentQueryText=""
+        if (checkResult){
+            setStepRotation()
+            return CheckResult.OK
+        }
+        else return CheckResult.KO
+    }
+
+
+    private fun checkCapitalLetterPrefix(string:String):Boolean{
         if (string.isNullOrEmpty()) return false
         return string[0].isLetter() && string[0].isUpperCase()
+    }
+
+    private fun setStepRotation(){
+        currentQueryText=""
+        currentStep=Step.ROTATION_ANGLE
+        stepTextIds=ROTATION_STEP_TEXTSID
     }
 
 }
